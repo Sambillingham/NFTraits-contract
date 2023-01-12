0a04b49edecd30792bf91d370de9b8614f686dbd
Sambillingham <hello@sambillingham.com>
2023-01-12 16:25:59 +0100

    add: withdraw function

--- contracts/NFTraits.sol
+++ contracts/NFTraits.sol
@@ -228,10 +228,22 @@ contract NFTraits is VRFV2WrapperConsumerBase, ERC1155, Ownable, ERC1155Supply {
         return levels.length;
     }
 
+    // utility function for checking chainlink VRF status
+    // function getStatus(uint256 requestId) public view returns (mintStatus memory)
+    // {
+    //     return statuses[requestId];
+    // }
+
     function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         internal
         override(ERC1155, ERC1155Supply)
     {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    function withdraw() public onlyOwner() {
+        uint256 balance = address(this).balance;
+        (bool success, ) = (msg.sender).call{ value: balance }("");
+        require(success, "Failed to widthdraw Ether");
     }
 }
