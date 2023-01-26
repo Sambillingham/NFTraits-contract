89c2de64f5e82219e2a29921c06c8122657aeefa
Sambillingham <hello@sambillingham.com>
2023-01-26 15:44:23 +0100

    add: MockBlergs should be ERC721 Enumarable

--- contracts/MockBlergs.sol
+++ contracts/MockBlergs.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.12;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MockBlergs is ERC721 {
+contract MockBlergs is ERC721, ERC721Enumerable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -17,5 +17,20 @@ contract MockBlergs is ERC721 {
     
         _tokenIds.increment();
     }
-    
+        
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
